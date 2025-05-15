1f72bbdca7de3e8a21cf63ddf7e2b6da69732645
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-05-15 19:00:52 +0530

    Applied

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let main = document.querySelector("#main");
-let crsr = document.querySelector("i");
-let clickCrsr = document.querySelector(".onClick");
-let clicked = false;
+var main = document.querySelector("#main");
+var cursor = document.querySelector("#cursor");
+var imageDiv = document.querySelector("#image");
 
 main.addEventListener("mousemove", function(dets){
-    crsr.style.left = dets.x+"px"
-    crsr.style.top = dets.y+"px"
-    clickCrsr.style.left = dets.x+"px"
-    clickCrsr.style.top = dets.y+"px"
+    gsap.to(cursor, {
+        x:dets.x,
+        y:dets.y,
+        duration:0.6,
+    })
+});
+
+imageDiv.addEventListener("mouseenter", function(){
+    cursor.innerHTML = "View More";
+    gsap.to(cursor, {
+        scale: 2,
+        backgroundColor:"#ffffff8a"
+    })
 })
 
-main.addEventListener("click", () => {
-    setTimeout(() => {
-        clicked = !clicked;
-    crsr.style.opacity = clicked ? 0 : 1;
-    clickCrsr.style.opacity = clicked ? 1 : 0;
-    }, 100);
+imageDiv.addEventListener("mouseleave", function(){
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+        backgroundColor:"#fff",
+    })
 })
